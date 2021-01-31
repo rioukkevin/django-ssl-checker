aa9dcd40adcabf684769198d3fba600a4421be6c
RIOU Kevin <riou.kkevin@gmail.com>
2021-01-31 18:32:54 +0100

    ✨feat (Websites): Add model for website

--- sslChecker/websites/models.py
+++ sslChecker/websites/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class WebsitesModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Date de création")
+    modified = models.DateTimeField(auto_now=True, verbose_name="Date de modification")
+    name = models.TextField(
+        primary_key=True,
+        verbose_name="Nom du site à inspecter",
+        blank=False,
+        editable=False,
+    )
+
+    class Meta:
+        abstract = False
+        ordering = ("-$name",)
